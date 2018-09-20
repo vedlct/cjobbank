6fe4fca7280aac4b60d8020f9dc544d41553ba7e
rumi <mujtaba.rumi1@gmail.com>
2018-09-20 14:21:20 +0600

    rumi

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,64 +1,11 @@
-<!DOCTYPE html>
+
 <html lang="en">
 <head>
-    <title>Bootstrap Example</title>
+    <title>Curriculam Vitae of {{$personalInfo->firstName}} {{$personalInfo->lastName}}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
    <style>
-       body {
-           background: none repeat scroll 0 0;
-       }
 
-
-
-       .logo img {
-           width: 80px;
-
-       }
-       .versity_name span {
-           color: red;
-       }
-
-       .application h3 {
-           color: red;
-           font-size: 25px;
-           margin-bottom: 30px;
-           text-align: center;
-           text-transform: uppercase;
-       }
-
-       .versity_name h2 {
-           font-size: 37px;
-           margin-left: 18px;
-       }
-       .application p {
-           margin: 0;
-           padding: 0;
-       }
-       .photo > p {
-           border: 1px solid;
-           height: 122px;
-           margin-top: 5px;
-           text-align: center;
-           width: 110px;
-       }
-
-       .personal {
-           border: 1px solid #000;
-           margin-top: 5px;
-           background-color: #B0DBF0;
-       }
-       .first_name {
-           -moz-border-bottom-colors: none;
-           -moz-border-left-colors: none;
-           -moz-border-right-colors: none;
-           -moz-border-top-colors: none;
-           border-color: -moz-use-text-color #000 #000;
-           border-image: none;
-           border-style: none solid solid;
-           border-width: medium 1px 1px;
-       }
        table, th, td {
            border: 1px solid black;
            border-collapse: collapse;
@@ -70,22 +17,18 @@
 
        input {
            border: medium none;
-
            padding: 0;
        }
        .label{
            background-color: #eff0f1;
        }
+       @page { margin-bottom: 0; }
    </style>
 
 </head>
-
-<style>
-    @page { size: auto;  margin: 5px; }
-</style>
 <body>
-<div class="structure">
-    <div style= "background: #fff; margin-bottom: 30px; " class="container">
+<div class="">
+    <div style="background: #fff; " class="">
 
         <table border="0" style="width:100%; margin-top: 10px; border: none;">
             <tr>
@@ -93,7 +36,7 @@
             </tr>
 
         </table>
-        <table border="0" style="width:100%; margin-top: 30px; border: none; border-bottom: 1px solid #000">
+        <table border="0" style="width:100%; margin-top: 30px; border: none;">
             <tr>
                 <td style="text-align: left; border: none;">
                     <h3 style="">{{$personalInfo->firstName}} {{$personalInfo->lastName}}</h3>
@@ -109,7 +52,7 @@
         </table>
 
 
-        <table border="0" style="width:100%; margin-top: 5px; border: none;">
+        <table border="0" style="width:100%; margin-top: 25px; border: none;">
             <tr>
                 <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000"><b>Education</b> </td>
             </tr>
@@ -117,62 +60,114 @@
 
 
     {{--Education--}}
-        @foreach($education as $edu)
-            <table border="0" style="width:100%; margin-top: 25px; border: none;">
+
+            <table border="0" style="width:100%; margin-top: 10px; ">
+                <thead>
                 <tr>
-
-                    <td style="border: none; width: 20%">Degree </td>
-                    <td style="border: none; width: 5%">:</td>
-
-                    <td style="border: none;"><b>{{$edu->educationLevelName}} in {{$edu->degreeName}}</b> </td>
+                    <th style="text-align: center" >Degree</th>
+                    <th style="text-align: center" >Institution</th>
+                    <th style="text-align: center" >Passing Year</th>
+                    <th style="text-align: center" >Result</th>
                 </tr>
-                <tr>
+                </thead>
+                <tbody >
+                @foreach($education as $edu)
+                    <tr>
+                        <td style="text-align: center"><b>{{$edu->educationLevelName}} in {{$edu->degreeName}}</b> </td>
+                        <td style="text-align: center">{{$edu->institutionName}}</td>
 
-                    <td style="border: none; width: 20%">Institution</td>
-                    <td style="border: none; width: 5%">:</td>
-                    <td style="border: none;">{{$edu->institutionName}}</td>
-                </tr>
-                <tr>
+                        <td style="text-align: center">{{$edu->passingYear}} </td>
 
-                    <td style="border: none; width: 20%">Passing Year</td>
-                    <td style="border: none; width: 5%">:</td>
-                    <td style="border: none;">{{$edu->passingYear}} </td>
-                </tr>
-                {{--<tr>--}}
+                        <td style="text-align: center"> {{$edu->result}}</td>
+                    </tr>
+                @endforeach
 
-                    {{--<td style="border: none; width: 20%">Board</td>--}}
-                    {{--<td style="border: none; width: 5%">:</td>--}}
-                    {{--<td style="border: none;">Dhaka</td>--}}
-                {{--</tr>--}}
+                </tbody>
 
-                <tr>
-
-                    <td style="border: none; width: 20%">Result</td>
-                    <td style="border: none; width: 5%">:</td>
-                    <td style="border: none; margin-bottom: 30px;"> {{$edu->result}}</td>
-                </tr>
 
             </table >
 
 
-
-
-        @endforeach
-
-
-
-
-
-
-
-        <table border="0" style="width:100%; margin-top: 5px; border: none;">
+        <table border="0" style="width:100%; margin-top: 15px; border: none;">
             <tr>
-                <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000"><b>Professional Certificate</b> </td>
+                <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000"><b>Job Experience</b> </td>
             </tr>
         </table>
 
-        @foreach($professionalCertificate as $certificate)
-            <table border="0" style="width:100%; margin-top: 25px; border: none;">
+
+            <table border="0" style="width:100%; margin-top: 10px; border: none;">
+
+                @php $count=1;@endphp
+                @foreach($jobExperience as $exp)
+
+                    <tr>
+                        <td width="2%" style="border: none; vertical-align: top">
+                            <span>{{$count++}}.</span>
+                        </td>
+
+
+                <td style="border: none;">
+
+                      Company Name : {{$exp->organization}} <br>
+                            Position: {{$exp->degisnation}} <br>
+                            Address: {{$exp->address}} <br>
+                            Duration: {{$exp->startDate}} -  @if($exp->endDate) {{$exp->endDate}} @else
+                                Continuing
+                            @endif
+                            .
+
+
+
+                    </td>
+                    </tr>
+
+                @endforeach
+
+            </table>
+
+
+        <table border="0" style="width:100%; margin-top: 15px; border: none;">
+            <tr>
+                <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000; background-color: #eff0f1;" ><b>Training Certificate</b> </td>
+            </tr>
+        </table>
+
+        <table border="0" style="width:100%; margin-top: 10px; border: none;">
+
+            @php $count=1;@endphp
+
+
+            @foreach($trainingCertificate as $certificate)
+                <tr>
+                <td width="2%" style="border: none; vertical-align: top">
+                    <span>{{$count++}}.</span>
+                </td>
+            <td style="border: none;">
+                Training Name : {{$certificate->trainingName}} <br>
+                    Vanue: {{$certificate->vanue}} <br>
+                    Duration: {{$certificate->startDate}} -  @if($certificate->endDate) {{$certificate->endDate}} @else
+                        Continuing
+                    @endif
+                    .
+
+
+
+            </td>
+                </tr>
+            @endforeach
+
+        </table>
+
+        <p style="page-break-after: always"></p>
+
+        <table border="0" style="width:100%;border: none;">
+            <tr>
+                <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000"> <b>Professional Certificate</b> </td>
+            </tr>
+        </table>
+
+        <table border="0" style="width:100%; margin-top: 10px; border: none;">
+            @foreach($professionalCertificate as $certificate)
                 <tr>
 
                     <td style="border: none; width: 20%">Certificate Name</td>
@@ -191,119 +186,67 @@
                     <td style="border: none; width: 5%">:</td>
                     <td style="border: none;">{{$certificate->startDate}} - {{$certificate->endDate}}</td>
                 </tr>
-                <tr>
 
+                <tr>
                     <td style="border: none; width: 20%">Status</td>
                     <td style="border: none; width: 5%">:</td>
-                    <td style="border: none; margin-bottom: 30px;">
-                        @if($certificate->status == 1) On Going @else
+                    <td style="border: none;">
+                        @if($certificate->status == 1) On Going
+                        @else
                             Completed
                         @endif
-                    
+
                     </td>
                 </tr>
+
                 <tr>
 
                     <td style="border: none; width: 20%">Result</td>
                     <td style="border: none; width: 5%">:</td>
-                    <td style="border: none; margin-bottom: 30px;">{{$certificate->result}}</td>
+                    <td style="border: none;">{{$certificate->result}}</td>
                 </tr>
 
-            </table >
+            @endforeach
+
+        </table >
 
 
-
-        @endforeach
-
-
-
-
-
-        <table border="0" style="width:100%; margin-top: 5px; border: none;">
-            <tr>
-                <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000"><b>Job Experience</b> </td>
-            </tr>
-        </table>
-
-        @foreach($jobExperience as $exp)
-            <table border="0" style="width:100%; margin-top: 5px; border: none;">
-
-                <td style="border: none;">
-                        <p>Company Name : {{$exp->organization}} <br>
-                            Position: {{$exp->degisnation}} <br>
-                            Address: {{$exp->address}} <br>
-                            Duration: {{$exp->startDate}} -  @if($exp->endDate) {{$exp->endDate}} @else
-                                Continuing
-                            @endif
-                            .
-
-                        </p>
-
-                    </td>
-
-            </table>
-
-        @endforeach
-
-
-
-
-        <table border="0" style="width:100%; margin-top: 5px; border: none;">
-            <tr>
-                <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000; background-color: #eff0f1;" ><b>Training Certificate</b> </td>
-            </tr>
-        </table>
-
-        @foreach($trainingCertificate as $certificate)
-
-        <table border="0" style="width:100%; margin-top: 25px; border: none;">
-            <td style="border: none;">
-                <p>Training Name : {{$certificate->trainingName}} <br>
-                    Vanue: {{$certificate->vanue}} <br>
-                    Duration: {{$certificate->startDate}} -  @if($certificate->endDate) {{$certificate->endDate}} @else
-                        Continuing
-                    @endif
-                    .
-
-                </p>
-
-            </td>
-
-        </table>
-        @endforeach
-
-
-
-        <table border="0" style="width:100%; margin-top: 5px; border: none;">
+        <table border="0" style="width:100%; margin-top: 15px; border: none;">
             <tr>
                 <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000"><b>Referee</b> </td>
             </tr>
         </table>
 
-        <table border="0" style="width:100%; margin-top: 5px; border: none;">
+        <table border="0" style="width:100%; margin-top: 10px; border: none;">
 
+            @php $count=1;@endphp
+
+
+            @foreach($refree as $ref)
                 <tr style="">
-                    @foreach($refree as $ref)
+
+                    <td width="2%" style="border: none; vertical-align: top">
+                        <span>{{$count++}}.</span>
+                    </td>
 
                     <td style="border: none;">
-                        <p> Name : {{$ref->firstName}} {{$ref->lastName}} <br>
+                        Name : {{$ref->firstName}} {{$ref->lastName}} <br>
                             Contact: {{$ref->phone}} <br>
                             Position: {{$ref->presentposition}} <br>
                             email: {{$ref->email}}
-                        </p>
-                    </td>
-                    @endforeach
 
+                    </td>
                 </tr>
+            @endforeach
+
+
 
 
         </table>
 
-
-
         <table border="0" style="width:100%; margin-top: 5px; border: none;">
             <tr>
-                <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000"><b>Relative in Caritaas</b> </td>
+                <td class="label" style="text-align: left; border: none; border-bottom: 1px solid #000"><b>Relative in Caritas</b> </td>
             </tr>
         </table>
 
@@ -311,7 +254,8 @@
 
         <table border="0" style="width:100%; margin-top: 5px; border: none;">
 
-            <tr style="">
+            <tr style=" border: none;">
+                @if($relativeCb->isEmpty())<td style=" border: none; text-align: center"> <strong>None </strong> </td> @endif
                 @foreach($relativeCb as $ref)
 
                     <td style="border: none;">
@@ -319,18 +263,13 @@
                             Position: {{$ref->degisnation}} <br>
                         </p>
                     </td>
+
                 @endforeach
 
             </tr>
 
 
         </table>
-
-
-
-
-
-
 
 
 
