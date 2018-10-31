3e104ae0dfb4af42b611cd042f371a90c6914bef
rumi <mujtaba.rumi1@gmail.com>
2018-10-31 14:43:06 +0600

    Farzad

--- resources/views/userCv/update/computerSkill.blade.php
+++ resources/views/userCv/update/computerSkill.blade.php
@@ -25,6 +25,11 @@
                                                 {{$skill->computerSkillName}}
                                             @endif
 
+                                                {{--@php--}}
+                                                    {{--array_push($skillId,$skill->id);--}}
+                                                    {{--array_push($skillTitle,$skill->computerSkillName );--}}
+                                                {{--@endphp--}}
+
                                             @endforeach
 
                                     </div>
@@ -152,10 +157,16 @@
 
 
             $("#addButton").click(function () {
+
+                {{--var skillId= <?php echo json_encode( $skillId ) ?>;--}}
+
                 if(counter>10){
                     alert("Only 10 Section allow per Time!!");
                     return false;
                 }
+
+
+
                 $("#btnPevious").hide();
 
 
@@ -254,6 +265,7 @@
                             $.ajax({
                                 type: "POST",
                                 url: '{{route('candidate.computerSkill.delete')}}',
+
                                 data: {_token:"{{csrf_token()}}",id: x},
                                 success: function (data) {
                                     $.alert({
@@ -307,3 +319,4 @@
 
 
 @endsection
+
