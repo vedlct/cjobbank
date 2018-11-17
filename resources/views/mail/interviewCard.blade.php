066eef7e46b8c7572ecdfaeebfb6b746c4cf9f86
rumi <mujtaba.rumi1@gmail.com>
2018-11-17 12:56:50 +0600

    rumi

--- resources/views/mail/interviewCard.blade.php
+++ resources/views/mail/interviewCard.blade.php
@@ -3,10 +3,15 @@
 <head>
     <style>
         #footer {
-            /*position: fixed;*/
-            bottom: 0;
-            width: 100%;
-            margin-bottom: 10px;
+
+            position:absolute;
+            bottom:0;
+
+
+        }
+        @page{
+
+            margin: 25px 20px;
         }
 
     </style>
@@ -16,8 +21,8 @@
 
     <table width="100%">
         <tr>
-            <td align="left" width="35%">
-                <table >
+            <td  width="35%">
+                <table align="left" >
                     <tr  >
                         <td> <b>Unternehmenszentrale: </b><br>
                             Mainzer Landstraße 49, 60329 Frankfurt am Main, DE<br>
@@ -72,8 +77,8 @@
 
         </td>
 
-        <td align="right"width="35%">
-            <table >
+        <td width="35%">
+            <table align="center" >
                 <tr>
                     <td> <b>Date: </b>Date</td>
                 </tr>
@@ -84,7 +89,7 @@
 </div>
 <hr>
 
-<div style="margin: 30px 30px 30px 30px;">
+<div style="margin: 0px 30px 30px 30px;">
 
     <table width="100%">
         <td align="left" width="35%">
@@ -101,7 +106,7 @@
 
 </div>
 
-<div style="margin: 10px 30px 30px 30px;">
+<div style="margin: 0px 30px 30px 30px;">
 
     <table width="100%">
         <td width="100%" align="left">
@@ -114,23 +119,18 @@
 
 </div>
 
-<div style="margin: 10px 30px 0px 30px;">
+<div style="margin: 0px 30px 0px 30px;">
 
     <table width="100%">
+        <tr>
         <td width="100%" align="left">
             <b>Dear</b>
             Mainzer Landstraße 49, 60329 Frankfurt am Main, DE
-            Phone: +4917686097107; Email: info@findhalal.de ,
         </td>
+        </tr>
+        <tr>
 
-    </table>
-
-</div>
-<div style="margin: 0px 30px 30px 30px;">
-
-    <table width="100%">
-
-        <td align="left" width="100%">
+            <td align="left" width="100%">
 
                         <span>
 
@@ -140,8 +140,9 @@
 
 <p>Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
                         </span>
-        </td>
+            </td>
 
+        </tr>
 
     </table>
 
@@ -149,11 +150,10 @@
 
 <div id="footer">
     <hr>
-
     <table width="100%">
         <tr>
             <td align="left" width="35%">
-                <table >
+                <table>
                     <tr  >
                         <td> <b>Unternehmenszentrale: </b><br>
                             Mainzer Landstraße 49, 60329 Frankfurt am Main, DE<br>
