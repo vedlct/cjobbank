be29c4f28244e0563a844f0d548fbe48cb29976d
Farzad <farzadrahman59@gmail.com>
2018-11-17 15:38:24 +0600

    Farzad

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Excel;
 
 class testController extends Controller
 {
@@ -23,5 +24,42 @@ class testController extends Controller
     public function printview($no){
 
         return  $no;
+    }
+
+
+    public function testExcel(){
+        $fileName="Full Info";
+
+//        return view('Admin.application.fullInfo');
+        $check=Excel::create($fileName,function($excel) {
+
+
+            $excel->sheet('First sheet', function($sheet) {
+
+                // Font family
+//                $sheet->setFontFamily('Comic Sans MS');
+
+// Set font with ->setStyle()`
+                $sheet->setStyle(array(
+                    'font' => array(
+                        'name'      =>  'Calibri',
+                        'size'      =>  10,
+                        'bold'      =>  false
+                    )
+                ));
+
+
+
+                $sheet->loadView('Admin.application.fullInfo');
+            });
+
+        });
+//        $check->setFontSize(15);
+
+        $check->download();
+
+//        $sheet->setFontSize(15);
+
+
     }
 }
---
+++ resources/views/Admin/application/fullInfo.blade.php
@@ -0,0 +1,234 @@
+<html>
+<head>
+    <style>
+        table{
+            font-size:5;
+        }
+    </style>
+
+</head>
+<body>
+
+
+<table class="table">
+
+    <tr>
+
+        <td colspan="8" style="text-align: center"></td>
+        <td colspan="8" style="text-align: center"></td>
+        <td colspan="8" style="text-align: center"></td>
+
+    </tr>
+    <tr >
+        <td colspan="24" style="vertical-align: center;height: 20;"><span style="text-align: center"><h4>CARITAS BANGLADESH</h4></span></td>
+    </tr>
+
+    <tr>
+
+        <td colspan="24" style="text-align: center;height: 15;">List of candidates for the position Of
+            <span id="red" style="color: #FF0000">Finance & Admin Officer For USAID's Improving Nutration Through Community- Based Approach (INCA)</span>
+        </td>
+
+    </tr>
+
+
+</table>
+
+<table class="table" style="font-size:10">
+    <tr style="font-weight: bold">
+        <td colspan="1" style="text-align: center">Sl No.</td>
+        <td colspan="4" style="text-align: center">Name and personal information</td>
+        <td colspan="3" style="text-align: center">Questionnaire</td>
+        <td colspan="4" style="text-align: center">Educational/ professional/Training Information</td>
+        <td colspan="4" style="text-align: center">Job Experiences/ Employment History</td>
+        <td colspan="4" style="text-align: center">Extracurricular activities/ other skills</td>
+        <td colspan="3" style="text-align: center">References</td>
+        <td colspan="1" style="text-align: center">Remarks</td>
+    </tr>
+    <tr>
+        <td colspan="1" height="250" style="text-align: center;vertical-align: middle;">1</td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;"><span style="text-align: center">Zamal Hossen</span></td>
+        <td colspan="3" height="250" style="text-align: center;vertical-align: middle;">Why Do You Want to Leave Your Current Job?</td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;">1. Degree title : M. Com (Accounting)<br>
+            Major Subject: B.Com (Ho)<br>
+            Passing Year : 2010<br>
+            Board<br>
+            Name of Institution/school/college/versity:<br>
+            Status (ongoing/Completed)-<br>
+            Result: 1st division<br><br>
+
+            2. Degree title : B. Com (Accounting)<br>
+            Major Subject: B.Com (Ho)<br>
+            Passing Year : 2010<br>
+            Name of Institution/school/college/versity: National University<br>
+            Result:<br>
+        </td>
+        <td colspan="4" height="250"   style="text-align: center;vertical-align: middle;">
+            1. Position:<br>
+            Organization name:<br>
+            Organizarion type:<br>
+            Total experience:<br>
+            Department:<br>
+            Type of Employment:<br>
+            Served From /To:<br>
+            Job location/Address:<br>
+            Name of Supervisor<br>:
+            Present Salary:<br>
+            Expected Salary:<br>
+            Ready to join after(Days):<br>
+        </td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;">
+            Extracurricular activities<br>
+            1. Interests<br>
+            2.Awards received<br>
+            3.Research/Publciation<br>
+
+
+            Other skills:<br>
+            Computer skill<br>
+            Language Proficiency<br>
+
+        </td>
+        <td colspan="3" height="250" style="text-align: center;vertical-align: middle;">
+            1. David Benjamin Singha <br>
+            Administrative Officer<br>
+            Christian Missionary Hospital<br>
+            Brahmanbaria, Bangladesh<br>
+            Cell Phone: 01703-095846               E-mail:davidbenjaminsingha@gmail.com<br>
+
+        </td>
+        <td colspan="1" height="250" style="text-align: center;vertical-align: middle;"></td>
+
+    </tr>
+    <tr>
+        <td colspan="1" height="250" style="text-align: center;vertical-align: middle;">2</td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;"><span style="text-align: center">
+                Telephone: 02146596461<br>
+                Personal Phone no.0215462162<br>
+                Alternative phone no 6645+9+9856+4<br>
+                Home Phone :0.2563<br>
+                Office Phone: 20256232<br>
+                Email: gfdydehy@gmail.com<br>
+                Alternative Email:-sdhiofhyo@yahoo.com<br>
+                Skype :zaman.1<br>
+                Date Of Birth: 7/12/1989<br>
+                National ID Card No-235856322020.<br>
+                Passport No. (If any)<br>
+                Parmanent Adress: Eden Garden(3rd floor)<br>
+                Main Road, Coxsbazar Sadar,<br>
+                Coxsbazar, Bangladesh.   <br>
+                Present Adress: Eden Garden(3rd floor)<br>
+                Main Road, Coxsbazar Sadar,<br>
+                Coxsbazar, Bangladesh.  <br>
+            </span></td>
+        <td colspan="3" height="250" style="text-align: center;vertical-align: middle;">
+            Why you are intersted for the position applied for?
+        </td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;">
+            Professional Qualification:<br>
+            1. Exam/Degree Title<br>
+            Subject/Concentration/Major<br>
+            Passing Year<br>
+            Name of Institution/school/college/versity<br>
+            Result<br>
+        </td>
+        <td colspan="4" height="250"   style="text-align: center;vertical-align: middle;">
+            Major Job responsibility:<br>
+            Key Achievement<br>
+
+
+            Previous work information in Caritas Bangladesh (If )<br>
+            Position in Caritas:<br>
+            Served from/to<br>
+        </td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;">
+
+        </td>
+        <td colspan="3" height="250" style="text-align: center;vertical-align: middle;">
+            1. Name of Referees, Designation, Organization, adress, mobile number and email number, Relation<br>
+            2. Name of Referees, Designation, Organization, adress, mobile number and email number, Relation
+        </td>
+        <td colspan="1" height="250" style="text-align: center;vertical-align: middle;"></td>
+
+    </tr>
+
+    <tr>
+        <td colspan="1" height="250" style="text-align: center;vertical-align: middle;">3</td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;"><span style="text-align: center">
+                Fathers Name<br>
+                Mothers Name<br>
+                Spouse Name: <br>
+                Blood Group<br>
+                Gender<br>
+                Nationality<br>
+                Ethnicity<br>
+                Disability(Yes/No)<br>
+                Religion<br>
+
+            </span></td>
+        <td colspan="3" height="250" style="text-align: center;vertical-align: middle;">
+            Do you have any relatives working in Caritas Bangladesh?*<br>
+            Name and relation:<br>
+
+            Answers of declaration form:<br>
+        </td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;">
+            Training information:<br>
+            1. Training/Workshop/Course title<br>
+            Institute<br>
+            Location of training/workshop<br>
+            Duration<br>
+            year<br>
+            Result (if any)<br>
+            Country<br>
+        </td>
+        <td colspan="4" height="250"   style="text-align: center;vertical-align: middle;">
+            Do you have any reservation contacting your employer?
+        </td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;">
+
+        </td>
+        <td colspan="3" height="250" style="text-align: center;vertical-align: middle;">
+
+        </td>
+        <td colspan="1" height="250" style="text-align: center;vertical-align: middle;"></td>
+
+    </tr>
+
+
+
+    <tr>
+        <td colspan="1" height="250" style="text-align: center;vertical-align: middle;">4</td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;"><span style="text-align: center">
+                Membership in Social Network :<br>
+                Name of Network<br>
+                Type of Membership<br>
+                Duration<br>
+
+            </span></td>
+        <td colspan="3" height="250" style="text-align: center;vertical-align: middle;">
+            Answers of declaration form:
+        </td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;">
+
+        </td>
+        <td colspan="4" height="250"   style="text-align: center;vertical-align: middle;">
+
+        </td>
+        <td colspan="4" height="250" style="text-align: center;vertical-align: middle;">
+
+        </td>
+        <td colspan="3" height="250" style="text-align: center;vertical-align: middle;">
+
+        </td>
+        <td colspan="1" height="250" style="text-align: center;vertical-align: middle;"></td>
+
+    </tr>
+
+
+
+</table>
+</body>
+
+
+</html>
